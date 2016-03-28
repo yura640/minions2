07f469bace1df37cd1781515683f430fe378a447
student <student@localhost.localdomain>
2016-03-28 12:22:19 +0300

    fix

--- require-js-learning/app/scripts/app.js
+++ require-js-learning/app/scripts/app.js
@@ -1,20 +1,24 @@
-define(['data','jquery'], function(data, $){
-    for (var i=0; i<myArr.length;i++){
-        var localStor = localStorage;
-        var oneStorageValue = localStor.getItem(myArr[i].id);
-        var currentCount;
-            if(oneStorageValue == null){
-                    currentCount = 0;
-                } else {
-                    currentCount = oneStorageValue
-                 }
+define(['jquery'], function($){
+    $.getJSON("./data/data.json", function(data){
+        $.each( data, function(i, elem) {
+            var localStor = localStorage;
+            var oneStorageValue = localStor.getItem(elem.id);
+            var currentCount;
+            if (oneStorageValue == null) {
+                currentCount = 0;
+            } else {
+                currentCount = oneStorageValue
+            }
 
-        $liElem = $('<li/>', {id: myArr[i].id, class: "item"});
-        $imgElem = $('<img/>', {src: "./images/"+myArr[i].image});
-        $spanElem = $('<span/>', {class: "counter", text: Number(currentCount)});
-        $liElem.append($imgElem, $spanElem);
-        $('.gallery').append($liElem);
+            $liElem = $('<li/>', {id: elem.id, class: "item"});
+            $imgElem = $('<img/>', {src: "./images/" + elem.image});
+            $spanElem = $('<span/>', {class: "counter", text: Number(currentCount)});
+            $liElem.append($imgElem, $spanElem);
+            $('.gallery').append($liElem);
 
-    }
-
+        });
+    })
+        .fail(function(){
+            console.error("Data Error")
+        })
 });
--- require-js-learning/app/scripts/main.js
+++ require-js-learning/app/scripts/main.js
@@ -6,11 +6,18 @@ require.config({
 });
 require(["jquery",'app'], function($, app){
 
-    $(".gallery").click(function(){
-        var curentTarget = event.target.parentNode;
-        var span = curentTarget.querySelector('span');
-        span.innerText = Number(span.innerText)+1;
-        localStorage.setItem(curentTarget.id, span.innerText);
-
+    $(".gallery").click(function(event){
+        var $curentTarget = $(event.target).parent();
+        var $span = $curentTarget.find('span');
+        $span.text(Number($span.text())+1);
+        localStorage.setItem($curentTarget.attr("id"), $span.text());
     });
+
+    //$(".gallery").click(function(){ // у мене цей варіант також робить...
+    //    var curentTarget = event.target.parentNode;
+    //    var span = curentTarget.querySelector('span');
+    //    span.innerText = Number(span.innerText)+1;
+    //    localStorage.setItem(curentTarget.id, span.innerText);
+    //
+    //});
 });
